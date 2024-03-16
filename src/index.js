18ffdfa33612101eb7f2c20f3e23a35ec466f72d
AnnaHort <annahort3d@gmail.com>
2024-03-16 13:33:49 +0200

    update homepage

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './components/scss/main.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/' >
+    <BrowserRouter basename='/sport-app-ignat' >
     <App />
     </BrowserRouter>
   </React.StrictMode>
 );
 
-reportWebVitals();
