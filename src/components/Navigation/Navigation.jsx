d65075d12afbd3a87f4f69f251c5d819163f0fbc
AnnaHort <annahort3d@gmail.com>
2024-03-16 17:42:42 +0200

    added navigation section & scss for it

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,9 +1,51 @@
-import React from 'react';
+import React from "react";
+import { SearchSvg } from "../svg/SearchSvg/SearchSvg";
+import { FavoriteSvg } from "../svg/FavoriteSvg/FavoriteSvg";
+import { BasketSvg } from "../svg/BasketSvg/BasketSvg";
+import "./navigation.scss";
+import { NavLink } from "react-router-dom";
 
 export const Navigation = () => {
   return (
     <section className="nav-section">
-      <h2>Navigation</h2>
+      <div className="nav-section-container container">
+        <h2 className="logo">Ignat</h2>
+
+        <nav className="navigation">
+          <ul className="navigation-list">
+            <li className="navigation-list-item">
+              <NavLink className="navigation-text">новинки</NavLink>
+            </li>
+            <li className="navigation-list-item">
+              <NavLink className="navigation-text">чоловіки</NavLink>
+            </li>
+            <li className="navigation-list-item">
+              <NavLink className="navigation-text">жінки</NavLink>
+            </li>
+            <li className="navigation-list-item">
+              <NavLink className="navigation-text">аксесуари</NavLink>
+            </li>
+            <li className="navigation-list-item">
+              <NavLink className="navigation-text">акції</NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="btn-container">
+          <div className="search-container">
+            <input type="text" className="input-search" />
+            <SearchSvg />
+          </div>
+          <NavLink>
+
+            <FavoriteSvg />
+          </NavLink>
+          <NavLink>
+   
+            <BasketSvg />
+          </NavLink>
+        </div>
+      </div>
     </section>
   );
 };
