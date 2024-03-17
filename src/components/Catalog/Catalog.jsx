13b115ab6705d4ee59925beac45481b75f8ff87e
AnnaHort <annahort3d@gmail.com>
2024-03-17 12:11:38 +0200

    added catalog section & scss style for it

---
+++ src/components/Catalog/Catalog.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import sportsman1x from "../../img/sportsman-1x.jpg";
+import sportsman2x from "../../img/sportsman-2x.jpg";
+import "./catalog.scss";
+
+export const Catalog = () => {
+  return (
+    <section className="catalog-section container">
+      <div className="enter-catalog">
+        <h2 className="catalog-title">
+          Ми знаємо, що сподобається вашим "великим" друзям!
+        </h2>
+        <p className="catalog-text">
+          Обирай подарунок своїм друзями бодібілдерам із нашою новою колекцію
+          термобілизни "Big warm"
+        </p>
+        <button className="enter-catalog-btn">До каталогу</button>
+      </div>
+      <picture>
+        <source srcSet={`${sportsman1x} 1x, ${sportsman2x} 2x`} />
+        <img src={sportsman1x} alt="sportsman" className="sportsman-img" />
+      </picture>
+    </section>
+  );
+};
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Hero } from "../../components/Hero/Hero";
+import { Catalog } from "../../components/Catalog/Catalog";
 
 const MainPage = () => {
   return (
     <main>
       <Hero />
+      <Catalog/>
     </main>
   );
 };
